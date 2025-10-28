d2d78481543ee3bbc8855d6c62febf7f201eafb2
Frank <frank.pfeift@protonmail.com>
2025-10-28 17:03:32 +0100

    fix: remove getEnv function

--- editors/builder-team-editor/config.ts
+++ editors/builder-team-editor/config.ts
@@ -1,26 +1,17 @@
-const getEnvVar = (key: keyof ImportMetaEnv, defaultValue: string): string => {
-  return import.meta.env[key] ?? defaultValue;
-};
-
 export const config = {
-  renownGraphqlEndpoint: getEnvVar(
-    "PH_CONNECT_VETRA_RENOWN_GRAPHQL_ENDPOINT",
-    "https://switchboard.renown-staging.vetra.io/graphql"
-  ),
-  vetraGraphqlEndpoint: getEnvVar(
-    "PH_CONNECT_VETRA_VETRA_GRAPHQL_ENDPOINT",
-    "https://switchboard.staging.vetra.io/graphql"
-  ),
-  renownProfileBasePath: getEnvVar(
-    "PH_CONNECT_VETRA_RENOWN_PROFILE_BASE_PATH",
-    "phd://renown-staging.vetra.to"
-  ),
-  vetraPackageBasePath: getEnvVar(
-    "PH_CONNECT_VETRA_VETRA_PACKAGE_BASE_PATH",
-    "phd://staging.vetra.to"
-  ),
-  vetraDriveBaseUrl: getEnvVar(
-    "PH_CONNECT_VETRA_DRIVE_BASE_URL",
-    "https://switchboard.staging.vetra.io/d"
-  ),
+  renownGraphqlEndpoint:
+    import.meta.env.PH_CONNECT_VETRA_RENOWN_GRAPHQL_ENDPOINT ||
+    "https://switchboard.renown-staging.vetra.io/graphql",
+  vetraGraphqlEndpoint:
+    import.meta.env.PH_CONNECT_VETRA_VETRA_GRAPHQL_ENDPOINT ||
+    "https://switchboard.staging.vetra.io/graphql",
+  renownProfileBasePath:
+    import.meta.env.PH_CONNECT_VETRA_RENOWN_PROFILE_BASE_PATH ||
+    "phd://renown-staging.vetra.to",
+  vetraPackageBasePath:
+    import.meta.env.PH_CONNECT_VETRA_VETRA_PACKAGE_BASE_PATH ||
+    "phd://staging.vetra.to",
+  vetraDriveBaseUrl:
+    import.meta.env.PH_CONNECT_VETRA_DRIVE_BASE_URL ||
+    "https://switchboard.staging.vetra.io/d",
 };
