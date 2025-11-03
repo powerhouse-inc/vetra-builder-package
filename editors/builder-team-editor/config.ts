4c79305fe9985c5dd5b59bb94f67dd209c005ce9
Frank <frank.pfeift@protonmail.com>
2025-11-03 11:50:09 +0100

    fix: env not found

--- editors/builder-team-editor/config.ts
+++ editors/builder-team-editor/config.ts
@@ -1,17 +1,17 @@
 export const config = {
   renownGraphqlEndpoint:
-    import.meta.env.PH_CONNECT_VETRA_RENOWN_GRAPHQL_ENDPOINT ||
-    "https://switchboard.renown-staging.vetra.io/graphql",
+    import.meta.env?.PH_CONNECT_VETRA_RENOWN_GRAPHQL_ENDPOINT ||
+    "https://switchboard.renown.vetra.io/graphql",
   vetraGraphqlEndpoint:
-    import.meta.env.PH_CONNECT_VETRA_VETRA_GRAPHQL_ENDPOINT ||
-    "https://switchboard.staging.vetra.io/graphql",
+    import.meta.env?.PH_CONNECT_VETRA_VETRA_GRAPHQL_ENDPOINT ||
+    "https://switchboard.vetra.io/graphql",
   renownProfileBasePath:
-    import.meta.env.PH_CONNECT_VETRA_RENOWN_PROFILE_BASE_PATH ||
-    "phd://renown-staging.vetra.to",
+    import.meta.env?.PH_CONNECT_VETRA_RENOWN_PROFILE_BASE_PATH ||
+    "phd://renown.vetra.to",
   vetraPackageBasePath:
-    import.meta.env.PH_CONNECT_VETRA_VETRA_PACKAGE_BASE_PATH ||
-    "phd://staging.vetra.to",
+    import.meta.env?.PH_CONNECT_VETRA_VETRA_PACKAGE_BASE_PATH ||
+    "phd://vetra.to",
   vetraDriveBaseUrl:
-    import.meta.env.PH_CONNECT_VETRA_DRIVE_BASE_URL ||
-    "https://switchboard.staging.vetra.io/d",
+    import.meta.env?.PH_CONNECT_VETRA_DRIVE_BASE_URL ||
+    "https://switchboard.vetra.io/d",
 };
