f27a12996b911e767d601291b4ef5572ecda69bd
Frank <frank.pfeift@protonmail.com>
2026-03-23 15:59:05 +0100

    chore: register new document model versions in index exports

--- document-models/document-models.ts
+++ document-models/document-models.ts
@@ -1,6 +1,10 @@
 import type { DocumentModelModule } from "document-model";
+import { BuilderAccount as BuilderAccountV1 } from "./builder-account/v1/module.js";
 import { BuilderTeam } from "./builder-team/module.js";
+import { BuilderTeam as BuilderTeamV1 } from "./builder-team/v1/module.js";
 
 export const documentModels: DocumentModelModule<any>[] = [
+  BuilderAccountV1,
   BuilderTeam,
+  BuilderTeamV1,
 ];
--- document-models/upgrade-manifests.ts
+++ document-models/upgrade-manifests.ts
@@ -1,4 +1,8 @@
 import type { UpgradeManifest } from "document-model";
+import { builderAccountUpgradeManifest } from "./builder-account/upgrades/upgrade-manifest.js";
+import { builderTeamUpgradeManifest } from "./builder-team/upgrades/upgrade-manifest.js";
 
 export const upgradeManifests: UpgradeManifest<readonly number[]>[] = [
+  builderAccountUpgradeManifest,
+  builderTeamUpgradeManifest,
 ];
